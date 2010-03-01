ea19c0a6a81a5eb8f96a4b078a383523a9cf40d3
L1L1@gmx.com <L1L1@gmx.com@b6bb1ec2-7570-11de-9207-fb4211ab666b>
2010-03-01 22:56:19 +0000

    Adapted to new ui layout (ui.tree_set_alt_value) and improved transaction log analysis.

--- trunk/dot_cardpeek_dir/scripts/emv.lua
+++ trunk/dot_cardpeek_dir/scripts/emv.lua
@@ -1,7 +1,7 @@
 --
 -- This file is part of Cardpeek, the smartcard reader utility.
 --
--- Copyright 2009 by 'L1L1'
+-- Copyright 2009-2010 by 'L1L1'
 --
 -- Cardpeek is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
@@ -40,85 +40,98 @@ end
 -- EMV 
 -- ------------------------------------------------------------------------
 
+TRANSACTION_TYPE = {
+  [0]="Purchase",
+  [1]="Cash"
+}
+
+function ui_parse_transaction_type(cc)
+	local tt = TRANSACTION_TYPE[cc[0]]
+	if tt == nil then 
+	   return tostring(cc)
+	end
+	return tt
+end
+
 EMV_REFERENCE = {
-   ['5D'] = "Directory Definition File (DDF) Name", 
-   ['70'] = "Application Data File (ADF)", 
-   ['80'] = "Response Message Template Format 1", 
-   ['82'] = "Application Interchange Profile (AIP)", 
-   ['87'] = "Application Priority Indicator", 
-   ['88'] = "Short File Identifier (SFI)", 
-   ['8C'] = "Card Risk Management Data Object List 1 (CDOL1)", 
-   ['8D'] = "Card Risk Management Data Object List 2 (CDOL2)", 
-   ['8E'] = "Cardholder Verification Method (CVM) List", 
-   ['8F'] = "Certificate Authority Public Key Index (PKI)", 
-   ['90'] = "Issuer PK Certificate", 
-   ['91'] = "Issuer Authentication Data", 
-   ['92'] = "Issuer PK Remainder", 
-   ['93'] = "Signed Static Application Data", 
-   ['94'] = "Application File Locator (AFL)",
-   ['95'] = "Terminal Verification Results (TVR)",
-   ['97'] = "Transaction Certificate Data Object List (TDOL)", 
-   ['9A'] = "Transaction Date",
-   ['9C'] = "Transaction Type",
-   ['A5'] = "FCI Proprietary Template", 
-   ['9F02'] = "Amount, Authorized",
-   ['9F03'] = "Amount, Other",
-   ['9F05'] = "Application Discretionary Data", 
-   ['9F07'] = "Application Usage Control", 
-   ['9F08'] = "Application Version Number", 
-   ['9F0B'] = "Cardholder Name - Extended", 
-   ['9F0D'] = "Issuer Action Code - Default", 
-   ['9F0E'] = "Issuer Action Code - Denial", 
-   ['9F0F'] = "Issuer Action Code - Online", 
-   ['9F10'] = "Issuer Application Data", 
-   ['9F11'] = "Issuer Code Table Index", 
-   ['9F12'] = "Application Preferred Name", 
-   ['9F13'] = "Last Online ATC Register", 
-   ['9F14'] = "Lower Consecutive Offline Limit (Terminal Check)", 
-   ['9F17'] = "PIN Try Counter", 
-   ['9F19'] = "Dynamic Data Authentication Data Object List (DDOL)", 
-   ['9F1A'] = "Terminal Country Code",
-   ['9F1F'] = "Track 1 Discretionary Data", 
-   ['9F23'] = "Upper Consecutive Offline Limit (Terminal Check)", 
-   ['9F26'] = "Application Cryptogram (AC)", 
-   ['9F27'] = "Cryptogram Information Data", 
-   ['9F2D'] = "ICC PIN Encipherment Public Key Certificate", 
-   ['9F2E'] = "ICC PIN Encipherment Public Key Exponent", 
-   ['9F2F'] = "ICC PIN Encipherment Public Key Remainder",
-   ['9F32'] = "Issuer PK Exponent", 
-   ['9F36'] = "Application Transaction Counter (ATC)", 
-   ['9F38'] = "Processing Options Data Object List (PDOL)", 
-   ['9F42'] = "Application Currency Code", 
-   ['9F44'] = "Application Currency Exponent", 
-   ['9F45'] = "Data Authentication Code", 
-   ['9F46'] = "ICC Public Key Certificate", 
-   ['9F47'] = "ICC Public Key Exponent", 
-   ['9F48'] = "ICC Public Key Remainder", 
-   ['9F4A'] = "Static Data Authentication Tag List", 
-   ['9F4B'] = "Signed Dynamic Application Data", 
-   ['9F4C'] = "Integrated Circuit Card (ICC) Dynamic Number",
-   ['9F4F'] = "Log Fromat",
-   ['9F51'] = "Application Currency Code",
-   ['9F52'] = "Card Verification Results (CVR)", 
-   ['9F53'] = "Consecutive Transaction Limit (International)", 
-   ['9F54'] = "Cumulative Total Transaction Amount Limit", 
-   ['9F55'] = "Geographic Indicator", 
-   ['9F56'] = "Issuer Authentication Indicator",
-   ['9F57'] = "Issuer Country Code", 
-   ['9F58'] = "Lower Consecutive Offline Limit (Card Check)",
-   ['9F59'] = "Upper Consecutive Offline Limit (Card Check)", 
-   ['9F5A'] = "Issuer URL2", 
-   ['9F5C'] = "Cumulative Total Transaction Amount Upper Limit", 
-   ['9F72'] = "Consecutive Transaction Limit (International - Country)", 
-   ['9F73'] = "Currency Conversion Factor", 
-   ['9F74'] = "VLP Issuer Authorization Code", 
-   ['9F75'] = "Cumulative Total Transaction Amount Limit - Dual Currency", 
-   ['9F76'] = "Secondary Application Currency Code", 
-   ['9F77'] = "VLP Funds Limit", 
-   ['9F78'] = "VLP Single Transaction Limit", 
-   ['9F79'] = "VLP Available Funds", 
-   ['9F7F'] = "Card Production Life Cycle (CPLC) History File Identifiers", 
-   ['BF0C'] = "FCI Issuer Discretionary Data"
+   ['5D'] = {"Directory Definition File (DDF) Name" }, 
+   ['70'] = {"Application Data File (ADF)" }, 
+   ['80'] = {"Response Message Template Format 1" }, 
+   ['82'] = {"Application Interchange Profile (AIP)" }, 
+   ['87'] = {"Application Priority Indicator" }, 
+   ['88'] = {"Short File Identifier (SFI)" }, 
+   ['8C'] = {"Card Risk Management Data Object List 1 (CDOL1)" }, 
+   ['8D'] = {"Card Risk Management Data Object List 2 (CDOL2)" }, 
+   ['8E'] = {"Cardholder Verification Method (CVM) List" }, 
+   ['8F'] = {"Certificate Authority Public Key Index (PKI)" }, 
+   ['90'] = {"Issuer PK Certificate" }, 
+   ['91'] = {"Issuer Authentication Data" }, 
+   ['92'] = {"Issuer PK Remainder" }, 
+   ['93'] = {"Signed Static Application Data" }, 
+   ['94'] = {"Application File Locator (AFL)" },
+   ['95'] = {"Terminal Verification Results (TVR)" },
+   ['97'] = {"Transaction Certificate Data Object List (TDOL)" }, 
+   ['9A'] = {"Transaction Date", ui_parse_YYMMDD },
+   ['9C'] = {"Transaction Type", ui_parse_transaction_type },
+   ['A5'] = {"FCI Proprietary Template" }, 
+   ['9F02'] = {"Amount, Authorized" },
+   ['9F03'] = {"Amount, Other" },
+   ['9F05'] = {"Application Discretionary Data" }, 
+   ['9F07'] = {"Application Usage Control" }, 
+   ['9F08'] = {"Application Version Number" }, 
+   ['9F0B'] = {"Cardholder Name - Extended" }, 
+   ['9F0D'] = {"Issuer Action Code - Default" }, 
+   ['9F0E'] = {"Issuer Action Code - Denial" }, 
+   ['9F0F'] = {"Issuer Action Code - Online" }, 
+   ['9F10'] = {"Issuer Application Data" }, 
+   ['9F11'] = {"Issuer Code Table Index" }, 
+   ['9F12'] = {"Application Preferred Name" }, 
+   ['9F13'] = {"Last Online ATC Register" }, 
+   ['9F14'] = {"Lower Consecutive Offline Limit (Terminal Check)" }, 
+   ['9F17'] = {"PIN Try Counter" }, 
+   ['9F19'] = {"Dynamic Data Authentication Data Object List (DDOL)" }, 
+   ['9F1A'] = {"Terminal Country Code", ui_parse_country_code },
+   ['9F1F'] = {"Track 1 Discretionary Data", ui_parse_printable }, 
+   ['9F23'] = {"Upper Consecutive Offline Limit (Terminal Check)" }, 
+   ['9F26'] = {"Application Cryptogram (AC)" }, 
+   ['9F27'] = {"Cryptogram Information Data" }, 
+   ['9F2D'] = {"ICC PIN Encipherment Public Key Certificate" }, 
+   ['9F2E'] = {"ICC PIN Encipherment Public Key Exponent" }, 
+   ['9F2F'] = {"ICC PIN Encipherment Public Key Remainder" },
+   ['9F32'] = {"Issuer PK Exponent" }, 
+   ['9F36'] = {"Application Transaction Counter (ATC)" }, 
+   ['9F38'] = {"Processing Options Data Object List (PDOL)" }, 
+   ['9F42'] = {"Application Currency Code" }, 
+   ['9F44'] = {"Application Currency Exponent" }, 
+   ['9F45'] = {"Data Authentication Code" }, 
+   ['9F46'] = {"ICC Public Key Certificate" }, 
+   ['9F47'] = {"ICC Public Key Exponent" }, 
+   ['9F48'] = {"ICC Public Key Remainder" }, 
+   ['9F4A'] = {"Static Data Authentication Tag List" }, 
+   ['9F4B'] = {"Signed Dynamic Application Data" }, 
+   ['9F4C'] = {"Integrated Circuit Card (ICC) Dynamic Number" },
+   ['9F4F'] = {"Log Fromat" },
+   ['9F51'] = {"Application Currency Code" },
+   ['9F52'] = {"Card Verification Results (CVR)" }, 
+   ['9F53'] = {"Consecutive Transaction Limit (International)" }, 
+   ['9F54'] = {"Cumulative Total Transaction Amount Limit" }, 
+   ['9F55'] = {"Geographic Indicator" }, 
+   ['9F56'] = {"Issuer Authentication Indicator" },
+   ['9F57'] = {"Issuer Country Code" }, 
+   ['9F58'] = {"Lower Consecutive Offline Limit (Card Check)" },
+   ['9F59'] = {"Upper Consecutive Offline Limit (Card Check)" }, 
+   ['9F5A'] = {"Issuer URL2" }, 
+   ['9F5C'] = {"Cumulative Total Transaction Amount Upper Limit" }, 
+   ['9F72'] = {"Consecutive Transaction Limit (International - Country)" }, 
+   ['9F73'] = {"Currency Conversion Factor" }, 
+   ['9F74'] = {"VLP Issuer Authorization Code" }, 
+   ['9F75'] = {"Cumulative Total Transaction Amount Limit - Dual Currency" }, 
+   ['9F76'] = {"Secondary Application Currency Code" }, 
+   ['9F77'] = {"VLP Funds Limit" }, 
+   ['9F78'] = {"VLP Single Transaction Limit" }, 
+   ['9F79'] = {"VLP Available Funds" }, 
+   ['9F7F'] = {"Card Production Life Cycle (CPLC) History File Identifiers" }, 
+   ['BF0C'] = {"FCI Issuer Discretionary Data" }
 }
 
 function emv_parse(cardenv,tlv)
@@ -150,6 +163,7 @@ function emv_process_pse(cardenv)
 	local REC
 	local RECORD
 	local aid
+	local warm_atr
 
         sw, resp = card.select(PSE)
 	
@@ -157,8 +171,9 @@ function emv_process_pse(cardenv)
 	if sw == 0x6E00 then 
 	   local ATR
 	   card.warm_reset()
-	   ATR = ui.tree_add_node(cardenv,"ATR","warm")
-	   ui.tree_set_value(ATR,tostring(card.last_atr()))
+	   warm_atr = tostring(card.last_atr())
+	   ATR = ui.tree_add_node(cardenv, "ATR", "warm", #warm_atr, "block")
+	   ui.tree_set_value(ATR,tostring(warm_atr))
            sw, resp = card.select(PSE)
 	end
 
@@ -168,20 +183,20 @@ function emv_process_pse(cardenv)
 	end
 
 	-- Construct tree
-	APP = ui.tree_add_node(cardenv,"application",PSE,nil,"PSE")
+	APP = ui.tree_add_node(cardenv,"application",PSE,nil,"application")
 	emv_parse(APP,resp)
 
 	ref = ui.tree_find_node(APP,nil,"88")
 	if (ref) then
 	   sfi = bytes.new(8,ui.tree_get_value(ref))
-	   FILE = ui.tree_add_node(APP,"file",sfi[0])
+	   FILE = ui.tree_add_node(APP,"file",sfi[0],nil,"file")
 
 	   rec = 1
 	   AID_LIST = {}
 	   repeat 
 	     sw,resp = card.read_record(sfi[0],rec)
 	     if sw == 0x9000 then
-	        RECORD = ui.tree_add_node(FILE,"record",tostring(rec))
+	        RECORD = ui.tree_add_node(FILE,"record",tostring(rec),nil,"record")
 	        emv_parse(RECORD,resp)
 	        aid = ui.tree_get_value(ui.tree_find_node(RECORD,nil,"4F"))
 	        table.insert(AID_LIST,"#"..aid)
@@ -202,7 +217,7 @@ function visa_process_application_logs(application, log_sfi, log_max)
 	local REC
 	local log_recno
 
-	LOG_FILE   = ui.tree_add_node(application,"file",tostring(log_sfi))
+	LOG_FILE   = ui.tree_add_node(application,"file",tostring(log_sfi),nil,"file")
 	LOG_DATA   = ui.tree_add_node(LOG_FILE,"log data")
  	
 	log.print(log.INFO,string.format("Reading LOG SFI %i",log_sfi))
@@ -212,7 +227,7 @@ function visa_process_application_logs(application, log_sfi, log_max)
                   log.print(log.WARNING,"Read log record failed")
 		  break
 	   else
-	          REC = ui.tree_add_node(LOG_DATA,"record",log_recno,#resp)
+	          REC = ui.tree_add_node(LOG_DATA,"record",log_recno,#resp,"record")
 		  ui.tree_set_value(REC,tostring(resp))
 	   end
 	end 
@@ -227,6 +242,7 @@ function emv_process_application_logs(application, log_sfi, log_max)
 	local sw, resp
 	local tag
 	local tag_name
+	local tag_func
 	local len
 	local i
 	local item
@@ -236,9 +252,12 @@ function emv_process_application_logs(application, log_sfi, log_max)
 	local LOG_FORMAT
 	local REC
 	local ITEM
+	local ITEM_AMOUNT
 	local log_recno
+	local data
+	local currency_code, currency_name, currency_digits
 
-	LOG_FILE   = ui.tree_add_node(application,"file",log_sfi)
+	LOG_FILE   = ui.tree_add_node(application,"file",log_sfi,nil,"file")
 	LOG_FORMAT = ui.tree_add_node(LOG_FILE,"log format")
 	LOG_DATA   = ui.tree_add_node(LOG_FILE,"log data")
  	
@@ -252,14 +271,15 @@ function emv_process_application_logs(application, log_sfi, log_max)
 
 	i = 1
 	item = ""
+	ui.tree_set_value(LOG_FORMAT,log_format);
 	while log_format 
 	do
 	    tag, log_format = asn1.split_tag(log_format)
 	    len, log_format = asn1.split_length(log_format)
-	    tag_name = tlv_tag_name(tag,EMV_REFERENCE,0);
-	    log_items[i]= { tag, len, tag_name }
+	    tag_name, tag_func = tlv_tag_info(tag,EMV_REFERENCE,0);
+	    log_items[i]= { tag, len, tag_name, tag_func }
 	    i = i+1
-	    item = item .. string.format("0x%X[%d] ", tag, len);
+	    item = item .. string.format("%X[%d] ", tag, len);
 	end
 	ui.tree_set_value(LOG_FORMAT,item);
 
@@ -270,18 +290,36 @@ function emv_process_application_logs(application, log_sfi, log_max)
                   log.print(log.WARNING,"Read log record failed")
 		  break
 	   else
-	          REC = ui.tree_add_node(LOG_DATA,"record",log_recno,#resp)
+	          REC = ui.tree_add_node(LOG_DATA,"record",log_recno,#resp,"record")
 		  item_pos = 0
+		  ITEM_AMOUNT = nil
+		  currency_digits = 0
 		  for i=1,#log_items do
 		     if log_items[i][3] then
 		        ITEM = ui.tree_add_node(REC,log_items[i][3],nil,log_items[i][2])
 		     else
 		        ITEM = ui.tree_add_node(REC,string.format("tag %X",log_items[i][1]),nil,log_items[i][2])
 		     end
-		     ui.tree_set_value(ITEM,tostring(bytes.sub(resp,item_pos,item_pos+log_items[i][2]-1)))
+
+		     data = bytes.sub(resp,item_pos,item_pos+log_items[i][2]-1)
+	             ui.tree_set_value(ITEM,tostring(data))
+
+		     if log_items[i][1]==0x9F02 then
+		        ITEM_AMOUNT=ITEM
+		     elseif log_items[i][1]==0x5F2A then
+	                currency_code   = tonumber(tostring(data))
+		        currency_name   = iso_currency_code_name(currency_code)
+	                currency_digits = iso_currency_code_digits(currency_code)
+	                ui.tree_set_alt_value(ITEM,currency_name)
+		     elseif log_items[i][4] then
+		        log_items[i][4](ITEM,data)
+                     end
 	             item_pos = item_pos + log_items[i][2]
 	          end
-		  ui.tree_set_value(REC,item)
+		  --ui.tree_set_value(REC,item)
+	          if ITEM_AMOUNT then
+		     ui.tree_set_alt_value(ITEM_AMOUNT,string.format("%.2f",ui.tree_get_value(ITEM_AMOUNT)/(10^currency_digits)))
+		  end
 	   end
 	end 
 
@@ -309,7 +347,7 @@ function emv_process_application(cardenv,aid)
 
 
 	-- Process 'File Control Infomation' and get PDOL
-	APP = ui.tree_add_node(cardenv,"application",aid)
+	APP = ui.tree_add_node(cardenv,"application",aid,nil,"application")
 	emv_parse(APP,resp)
 	ref = ui.tree_find_node(APP,nil,"9F38")
 	if (ref) then
@@ -379,14 +417,14 @@ function emv_process_application(cardenv,aid)
 	    local sfi
 	    local rec
 	    log.print(log.INFO,string.format("Reading SFI %i",bit.SHR(AFL[i],3)))
-	    sfi = ui.tree_add_node(APP,"file",bit.SHR(AFL[i],3))
+	    sfi = ui.tree_add_node(APP,"file",bit.SHR(AFL[i],3),nil,"file")
 	    for j=AFL[i+1],AFL[i+2] do
 		log.print(log.INFO,string.format("Reading record %i",j))
 		sw,resp = card.read_record(bit.SHR(AFL[i],3),j)
 	        if sw ~= 0x9000 then
 		   log.print(log.ERROR,"Read record failed")
 		else
-		   rec = ui.tree_add_node(sfi,"record",j)
+		   rec = ui.tree_add_node(sfi,"record",j,nil,"record")
 		   emv_parse(rec,resp)
 		end
 	    end
@@ -449,7 +487,7 @@ function emv_process_cplc(cardenv)
 	log.print(log.INFO,"Processing CPLC data")
 	sw,resp = card.get_data(0x9F7F)
 	if sw == 0x9000 then
-	   CPLC      = ui.tree_add_node(cardenv,"cpcl")
+	   CPLC      = ui.tree_add_node(cardenv,"cpcl",nil,nil,"block")
 	   cplc_tag, cplc_data = asn1.split(resp)
 	   ref = ui.tree_add_node(CPLC,"cplc data","9F7F",#cplc_data)
 	   ui.tree_set_value(ref,tostring(cplc_data)) 
@@ -457,9 +495,7 @@ function emv_process_cplc(cardenv)
 	   for i=1,#CPLC_DATA do
 	       ref2 = ui.tree_add_node(CPLC,
 				       CPLC_DATA[i][1],
-				       nil,
-				       nil,
-				       string.format("pos=%i",pos))
+				       pos);
 	       ui.tree_set_value(ref2,tostring(bytes.sub(cplc_data,pos,pos+CPLC_DATA[i][2]-1)))
 	       pos = pos + CPLC_DATA[i][2]
 	   end

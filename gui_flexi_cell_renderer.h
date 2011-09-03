beaf4797d6d48e7bf421a3cc1a28be65a1d15c0e
L1L1 <L1L1@gmx.com>
2011-09-03 17:15:19 +0000

    A custom cell renderer for the cardpeek treeview, for future evolutions of cardpeek. This code is not used in v0.7

---
+++ gui_flexi_cell_renderer.h
@@ -0,0 +1,47 @@
+#ifndef GUI_FLEXI_CELL_RENDERER_H
+#define GUI_FLEXI_CELL_RENDERER_H
+
+/* This file is WORK IN PROGRESS  */
+/* it has not use in cardpeek yet */
+
+
+#include <gtk/gtk.h>
+
+
+#define CUSTOM_TYPE_CELL_RENDERER_FLEXI             (custom_cell_renderer_flexi_get_type())
+#define CUSTOM_CELL_RENDERER_FLEXI(obj)             (G_TYPE_CHECK_INSTANCE_CAST((obj),  CUSTOM_TYPE_CELL_RENDERER_FLEXI, CustomCellRendererFlexi))
+#define CUSTOM_CELL_RENDERER_FLEXI_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass),  CUSTOM_TYPE_CELL_RENDERER_FLEXI, CustomCellRendererFlexiClass))
+#define CUSTOM_IS_CELL_FLEXI_FLEXI(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), CUSTOM_TYPE_CELL_RENDERER_FLEXI))
+#define CUSTOM_IS_CELL_FLEXI_FLEXI_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass),  CUSTOM_TYPE_CELL_RENDERER_FLEXI))
+#define CUSTOM_CELL_RENDERER_FLEXI_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj),  CUSTOM_TYPE_CELL_RENDERER_FLEXI, CustomCellRendererFlexiClass))
+
+typedef struct _CustomCellRendererFlexi CustomCellRendererFlexi;
+typedef struct _CustomCellRendererFlexiClass CustomCellRendererFlexiClass;
+
+/* CustomCellRendererFlexi: Our custom cell renderer
+ *   structure. Extend according to need */
+
+struct _CustomCellRendererFlexi
+{
+  GtkCellRenderer   parent;
+
+  gboolean	interpret;
+  char		*text;
+  char		*fallback;
+  char  	*mime_type;
+  /* GValue	*value; */ 
+  /* PangoAttrList *text_attr; */
+};
+
+
+struct _CustomCellRendererFlexiClass
+{
+  GtkCellRendererClass  parent_class;
+};
+
+GType                custom_cell_renderer_flexi_get_type (void);
+
+GtkCellRenderer     *custom_cell_renderer_flexi_new (gboolean interpret);
+
+
+#endif

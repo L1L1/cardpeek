89cf8f47c7db72a0ac06bfa38a7ce04b3fb764a4
L1L1 <L1L1@gmx.com>
2013-04-01 15:07:54 +0000

    Moved a_string functions out of misc.c to a_string.c

--- misc.h
+++ misc.h
@@ -58,31 +58,6 @@ extern const char ANSI_MAGENTA[];
 extern const char ANSI_CYAN[];
 extern const char ANSI_WHITE[];
 
-typedef struct {
-  unsigned _alloc;
-  unsigned _size;
-  char*    _data;
-} a_string_t;
-
-a_string_t* a_strnnew(unsigned n, const char* str);
-a_string_t* a_strnew(const char* str);
-void a_strfree(a_string_t* cs);
-char* a_strfinalize(a_string_t* cs);
-
-const char* a_strncpy(a_string_t* cs, unsigned n, const char* str);
-const char* a_strcpy(a_string_t* cs, const char* str);
-const char* a_strncat(a_string_t* cs, unsigned n, const char* str);
-const char* a_strcat(a_string_t* cs, const char* str);
-
-const char* a_strpushback(a_string_t* cs, char c);
-
-char* a_strval(a_string_t* cs);
-unsigned a_strlen(const a_string_t* cs);
-
-int a_sprintf(a_string_t* cs, const char *format, ...);
-int a_strnequal(const a_string_t* cs, unsigned n, const char *value);
-int a_strequal(const a_string_t* cs, const char *value);
-
 guint cstring_hash(gconstpointer data);
 gint cstring_equal(gconstpointer a, gconstpointer b);
 

d00dd3b004b9283afd0ca97fba007327f97fb5ed
L1L1@gmx.com <L1L1@gmx.com@b6bb1ec2-7570-11de-9207-fb4211ab666b>
2013-04-18 20:37:15 +0000

    Changed the bytestring lua API to make it simpler and (optionaly) object oriented.

--- trunk/lua_bytes.c
+++ trunk/lua_bytes.c
@@ -2,7 +2,7 @@
 *
 * This file is part of Cardpeek, the smartcard reader utility.
 *
-* Copyright 2009-2010 by 'L1L1'
+* Copyright 2009-2013 by 'L1L1'
 *
 * Cardpeek is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -27,64 +27,55 @@
 
 /*** LUA_BYTES: lua encapsulation of bytestring.c ***/
 
-bytestring_t* luaL_checkbytestring(lua_State *L, int p)
+bytestring_t* luaL_check_bytestring(lua_State *L, int p)
 {
   void *ud = luaL_checkudata(L,p,"bytes.type");
   luaL_argcheck(L, ud != NULL, p, "`bytes' expected");
-  return (bytestring_t *)ud;
+  return *(bytestring_t **)ud;
 }
 
-#ifdef _DEAD_CODE
-static void stack_dump(lua_State *L)
+int luaL_is_bytestring(lua_State *L, int idx)
 {
-  unsigned i;
+	int retval;
+	void *p;
+	
+	if (idx<0) idx=lua_gettop(L)+idx+1;
 
+	if ((p = lua_touserdata(L, idx)) == NULL)
+	{	
+		return 0;
+	}
 
-  for (i=1;i<=lua_gettop(L);i++)
-  {
-    fprintf(stderr,"[%d] %s ", i, lua_typename(L,lua_type(L,i)));
-    switch (lua_type(L,i)) {
-      case LUA_TUSERDATA:
-	fprintf(stderr,"%p\n",lua_touserdata(L,i));
-	break;
-      case LUA_TSTRING:
-	fprintf(stderr,"%s\n",lua_tostring(L,i));
-        break;
-      default:
-	fprintf(stderr,"\n");
-    }
-  }
-}
-#endif
+	luaL_getmetatable(L, "bytes.type");  /* pushes one on stack */
 
-static bytestring_t* luaL_newbytestring(lua_State *L, unsigned width)
-{
-  bytestring_t *bs;
-  bs = (bytestring_t *)lua_newuserdata(L, sizeof(bytestring_t));
-  if (bytestring_init(bs,width)!=BYTESTRING_OK)
-  {
-    lua_pop(L,1);
-    lua_pushnil(L);
-    return NULL;
-  }
-  luaL_getmetatable(L, "bytes.type");
-  lua_setmetatable(L, -2);
-  return bs;
+	if (!lua_getmetatable(L, idx))
+	{
+		lua_pop(L,1);
+		return 0;
+	}
+
+	if (!lua_rawequal(L, -1, -2))
+		retval = 0;
+	else
+		retval = 1;
+
+	lua_pop(L, 2);  
+ 
+	return retval;
 }
 
-void lua_pushbytestring(lua_State *L, bytestring_t* bs)
+void lua_push_bytestring(lua_State *L, bytestring_t* bs)
 {
-  bytestring_t *dst;
+  bytestring_t **dst;
 
   if (bs==NULL)
     lua_pushnil(L);
   else
   {
-    dst = (bytestring_t *)lua_newuserdata(L, sizeof(bytestring_t));
-    luaL_getmetatable(L, "bytes.type");
+    dst = (bytestring_t **)lua_newuserdata(L, sizeof(bytestring_t *)); /* -2 */
+    luaL_getmetatable(L, "bytes.type"); /* -1 */
     lua_setmetatable(L, -2);
-    memcpy(dst,bs,sizeof(bytestring_t));
-    free(bs);
+    *dst = bs;
   }
 }
 
@@ -107,7 +98,7 @@ static bytestring_t* x_bytes_create(lua_State *L, unsigned width, int start, int
 	bytestring_pushback(bs,lua_tointeger(L,p));
 	break;
       case LUA_TUSERDATA:
-	tmp = luaL_checkbytestring(L,p);
+	tmp = luaL_check_bytestring(L,p);
 	bytestring_append(bs,tmp);
 	break;
       default:
@@ -118,19 +109,19 @@ static bytestring_t* x_bytes_create(lua_State *L, unsigned width, int start, int
   return bs;
 }
 
+/*******************************************************
+ *
+ * CONSTRUCTORS
+ */
+
 static int subr_bytes_new(lua_State *L) 
 {
   int width = luaL_checkint(L, 1);
   int n = lua_gettop(L);
   bytestring_t *bs;
-  bytestring_t *tmp;
 
-  bs=luaL_newbytestring(L,width);
-  if (bs==NULL)
-    return 1;
-  tmp=x_bytes_create(L,width,2,n);
-  bytestring_copy(bs,tmp);
-  bytestring_free(tmp);
+  bs=x_bytes_create(L,width,2,n);
+  lua_push_bytestring(L,bs);
   return 1;
 }
 
@@ -141,76 +132,244 @@ static int subr_bytes_new_from_chars(lua_State *L)
   
   bytestring_assign_data(bs,strlen(str),(const unsigned char *)str);
 
-  lua_pushbytestring(L,bs);
+  lua_push_bytestring(L,bs);
   return 1;
 }
 
-static int subr_bytes_assign(lua_State *L)
+static int subr_bytes_concat(lua_State *L)
 {
-  bytestring_t *bs  = luaL_checkbytestring(L, 1);
-  bytestring_t *tmp = x_bytes_create(L,bs->width,2,lua_gettop(L));
-  lua_pushboolean(L,bytestring_copy(bs,tmp)==BYTESTRING_OK);
-  bytestring_free(tmp);
+  int i;
+  bytestring_t *bs;
+  int width = 8;
+
+  /* find the first bytestring and get its width */
+  for (i=1;i<=lua_gettop(L);i++)
+  {
+    if (lua_isuserdata(L,i))
+    {
+      bs = luaL_check_bytestring(L, i);
+      width = bs->width;
+      break;
+    }
+  }
+
+  /* create the result and push it on the stack */
+  bs = x_bytes_create(L,width,1,lua_gettop(L)); 
+  lua_push_bytestring(L,bs);
   return 1;
 }
 
-static int subr_bytes_append(lua_State *L)
+/*******************************************************
+ *
+ * DESTRUCTOR
+ */
+
+static int subr_bytes_gc(lua_State *L)
 {
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
-  bytestring_t *tmp = x_bytes_create(L,bs->width,2,lua_gettop(L));
-  lua_pushboolean(L,bytestring_append(bs,tmp)==BYTESTRING_OK);
-  bytestring_free(tmp);
+  bytestring_t **bs=(bytestring_t **)lua_touserdata(L,1);
+	
+  if (*bs)
+    bytestring_free(*bs);
+  return 0;
+}
+
+/*******************************************************
+ *
+ * META-METHODS
+ */ 
+
+static int subr_bytes_tostring(lua_State *L) 
+{
+  bytestring_t *bs = luaL_check_bytestring(L, 1);
+  char *s=bytestring_to_format("%D",bs);
+  lua_pushstring(L,s);
+  free(s);
   return 1;
 }
 
-static int subr_bytes_insert(lua_State *L)
+static int subr_bytes_len(lua_State *L)
 {
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
-  int pos = luaL_checkint(L,2);
-  bytestring_t *tmp = x_bytes_create(L,bs->width,3,lua_gettop(L));
-  if (pos<0) 
-    pos=bytestring_get_size(bs)+pos;
-  lua_pushboolean(L,bytestring_insert(bs,pos,tmp)==BYTESTRING_OK);
-  bytestring_free(tmp);
+  bytestring_t *bs = luaL_check_bytestring(L, 1);
+  lua_pushnumber(L,bytestring_get_size(bs));
   return 1;
 }
+
+
+static int subr_bytes_eq(lua_State *L)
+{
+  bytestring_t *a = luaL_check_bytestring(L, 1);
+  bytestring_t *b = luaL_check_bytestring(L, 2);
+
+  lua_pushboolean(L,bytestring_is_equal(a,b));
+  return 1;
+}
+
+/******************************************************
+ *
+ * ITERATOR
+ */
+
+typedef struct {
+	bytestring_t* bs;
+	unsigned      index;
+} bytes_iter_data_t;
+
+static int iter_bytes(lua_State *L)
+{
+	unsigned char c;
+
+	bytes_iter_data_t *iter = (bytes_iter_data_t *)lua_touserdata(L,lua_upvalueindex(1));
+
+	if (iter->index<bytestring_get_size(iter->bs))
+	{
+		lua_pushnumber(L,iter->index);
+		bytestring_get_element(&c,iter->bs,iter->index++);
+		lua_pushnumber(L,c);
+		return 2;
+	}
+	return 0;
+}
+
+static int subr_bytes_ipairs(lua_State *L)
+{
+	bytestring_t *bs = luaL_check_bytestring(L, 1);
+	bytes_iter_data_t *iter = (bytes_iter_data_t *)lua_newuserdata(L,sizeof(bytestring_t));
+
+	iter->bs = bs;
+	iter->index = 0;
+
+	lua_pushcclosure(L, iter_bytes, 1);
+        return 1;
+}
+
+/********************************************************
+ * 
+ * ORDINARY METHODS
+ */
+
+static int subr_bytes_get(lua_State *L)
+{
+  bytestring_t *bs = luaL_check_bytestring(L, 1);
+  int b_index;
+  int e_index;
+  int i;
+  unsigned char c;
+
+  if (lua_isnoneornil(L,2))
+	b_index = 0;
+  else
+  {
+	b_index=luaL_checkint(L,2);
+	if (b_index<0) b_index = 0;
+  }
+
+  if (lua_isnoneornil(L,3))
+	e_index = b_index;
+  else
+  {
+	e_index=luaL_checkint(L,2);
+	if (e_index>=(int)bytestring_get_size(bs)) e_index = bytestring_get_size(bs)-1;
+  }
+
+  if (e_index<b_index)
+  {
+	lua_pushnil(L);
+        return 1;
+  }
+
+  for (i=b_index;i<=e_index;i++)
+  {
+	bytestring_get_element(&c,bs,i);
+	lua_pushnumber(L,c);
+  }
+  return e_index-b_index+1;
+}
+
+static int subr_bytes_set(lua_State *L)
+{
+  bytestring_t *bs = luaL_check_bytestring(L, 1);
+  unsigned b_index;
+  unsigned e_index;
+  unsigned i;
+  unsigned char c;
+
+  b_index=(unsigned)luaL_checkint(L,2);
+
+  e_index = b_index + lua_gettop(L)-3;
+
+  for (i=b_index;i<=e_index;i++)
+  {
+	  c = (unsigned char)luaL_checkint(L,i-b_index+3);
+	  if (i<bytestring_get_size(bs))
+	  {
+		  bytestring_set_element(bs,i,c);
+	  }
+	  else if (i==bytestring_get_size(bs))
+	  {
+		  bytestring_pushback(bs,c);
+	  }
+  }
+  lua_settop(L,1);
+  return 1;
+}
+
+
 
 static int subr_bytes_pad_left(lua_State *L)
 {
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
+  bytestring_t *bs = bytestring_duplicate(luaL_check_bytestring(L, 1));
   unsigned pad_length = luaL_checkint(L,2);
   unsigned pad_value =  luaL_checkint(L,3);
-  lua_pushboolean(L,bytestring_pad_left(bs,pad_length,pad_value)==BYTESTRING_OK);
+  if (bytestring_pad_left(bs,pad_length,pad_value)!=BYTESTRING_OK)
+  {
+	bytestring_free(bs);
+	lua_pushnil(L);
+	return 1;
+  }
+  lua_push_bytestring(L,bs);
   return 1;
 }  
+
 
 static int subr_bytes_pad_right(lua_State *L)
 {
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
+  bytestring_t *bs = bytestring_duplicate(luaL_check_bytestring(L, 1));
   unsigned pad_length = luaL_checkint(L,2);
   unsigned pad_value =  luaL_checkint(L,3);
-  lua_pushboolean(L,bytestring_pad_right(bs,pad_length,pad_value)==BYTESTRING_OK);
+  if (bytestring_pad_right(bs,pad_length,pad_value)!=BYTESTRING_OK)
+  {
+	bytestring_free(bs);
+	lua_pushnil(L);
+	return 1;
+  }
+  lua_push_bytestring(L,bs);
   return 1;
 }  
 
-static int subr_bytes_invert(lua_State *L)
+static int subr_bytes_reverse(lua_State *L)
 {
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
-  lua_pushboolean(L,bytestring_invert(bs)==BYTESTRING_OK);
+  bytestring_t *bs = bytestring_duplicate(luaL_check_bytestring(L, 1));
+  if (bytestring_invert(bs)!=BYTESTRING_OK)
+  {
+	bytestring_free(bs);
+	lua_pushnil(L);
+	return 1;
+  }
+  lua_push_bytestring(L,bs);
   return 1;
 }  
 
 static int subr_bytes_clone(lua_State *L)
 {
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
-  bytestring_t *res = luaL_newbytestring(L,bs->width);
-  bytestring_copy(res,bs);
+  bytestring_t *bs = luaL_check_bytestring(L, 1);
+  bytestring_t *res = bytestring_duplicate(bs);
+  lua_push_bytestring(L,res);
   return 1;
 }
 
 static int subr_bytes_sub(lua_State *L)
 {
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
+  bytestring_t *bs = luaL_check_bytestring(L, 1);
   int start=luaL_checkint(L,2);
   int end=-1;
   bytestring_t *ret;
@@ -222,157 +381,37 @@ static int subr_bytes_sub(lua_State *L)
   if (end<0)
     end=bytestring_get_size(bs)+end;
 
-  ret = luaL_newbytestring(L,bs->width);
+  ret = bytestring_new(bs->width);
 
   if (bytestring_substr(ret,start,end-start+1,bs)!=BYTESTRING_OK)
   {
-    lua_pop(L,1);
+    bytestring_free(ret);
     lua_pushnil(L);
+    return 1;
   }
-  return 1;
-}
-
-static int subr_bytes_remove(lua_State *L)
-{
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
-  int start=luaL_checkint(L,2);
-  int end=-1;
-
-  if (lua_gettop(L)>2)
-    end=luaL_checkint(L,3);
-  if (start<0)
-    start=bytestring_get_size(bs)+start;
-  if (end<0)
-    end=bytestring_get_size(bs)+end;
-
-  lua_pushboolean(L,bytestring_erase(bs,start,end-start+1)==BYTESTRING_OK);
-  return 1;
-}
-
-static int subr_bytes_gc(lua_State *L)
-{
-  bytestring_t *bs=lua_touserdata(L,1);
-  /* fprintf(stderr,"lua is garbage collecting %p\n",(void*)bs); */
-  bytestring_release(bs);
-  return 0;
-}
-
-static int subr_bytes_tostring(lua_State *L) 
-{
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
-  char *s=bytestring_to_format("%D",bs);
-  lua_pushstring(L,s);
-  free(s);
-  return 1;
-}
-
-static int subr_bytes_format(lua_State *L)
-{
-  const char *format = lua_tostring(L,1);
-  bytestring_t *bs = luaL_checkbytestring(L,2);
-  char *s=bytestring_to_format(format,bs);
-  lua_pushstring(L,s);
-  free(s);
-  return 1;
-}
-
-static int subr_bytes_index(lua_State *L)
-{
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
-  int b_index=luaL_checkint(L,2);
-  unsigned char c;
-
-  if (bytestring_get_element(&c,bs,b_index)!=BYTESTRING_OK)
-  {
-    log_printf(LOG_WARNING,"bytestring index must be between 0 and %d",bytestring_get_size(bs)-1);
-    return luaL_error(L,"index %d is out of range",b_index);
-  }
-  lua_pushnumber(L,c);
-  return 1;
-}
-
-static int subr_bytes_newindex(lua_State *L)
-{
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
-  int b_index=luaL_checkint(L,2);
-  unsigned val=luaL_checkint(L,3);
-
-  if (bytestring_set_element(bs,b_index,val)!=BYTESTRING_OK)
-    return luaL_error(L,"index %d out of range",b_index);
-  lua_pushnumber(L,val);
-  return 1;
-}
-
-static int subr_bytes_len(lua_State *L)
-{
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
-  lua_pushnumber(L,bytestring_get_size(bs));
+  
+  lua_push_bytestring(L,ret);
   return 1;
 }
 
 static int subr_bytes_width(lua_State *L)
 {
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
+  bytestring_t *bs = luaL_check_bytestring(L, 1);
   lua_pushnumber(L,bs->width);
-  return 1;
-}
-
-static int subr_bytes_maxn(lua_State *L)
-{
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
-  if (bytestring_get_size(bs)==0)
-    lua_pushnil(L);
-  else
-    lua_pushnumber(L,bytestring_get_size(bs)-1);
-  return 1;
-}
-
-
-
-static int subr_bytes_concat(lua_State *L)
-{
-  int i;
-  bytestring_t *bs;
-  int width = 8;
-
-  /* find the first bytestring and get its width */
-  for (i=1;i<=lua_gettop(L);i++)
-  {
-    if (lua_isuserdata(L,1))
-    {
-      bs = luaL_checkbytestring(L, i);
-      width = bs->width;
-      break;
-    }
-  }
-
-  /* create the result and push it on the stack */
-  bs = x_bytes_create(L,width,1,lua_gettop(L)); 
-  lua_pushbytestring(L,bs);
-  return 1;
-}
-
-static int subr_bytes_eq(lua_State *L)
-{
-  bytestring_t *a = luaL_checkbytestring(L, 1);
-  bytestring_t *b = luaL_checkbytestring(L, 2);
-
-  lua_pushboolean(L,bytestring_is_equal(a,b));
   return 1;
 }
 
 static int subr_bytes_is_printable(lua_State *L)
 {
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
+  bytestring_t *bs = luaL_check_bytestring(L, 1);
 
   lua_pushboolean(L,bytestring_is_printable(bs)==BYTESTRING_OK);
   return 1;
 }
 
-
 static int subr_bytes_to_printable(lua_State *L)
 {
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
+  bytestring_t *bs = luaL_check_bytestring(L, 1);
   char *ret=bytestring_to_format("%P",bs);
   
   lua_pushstring(L,ret);
@@ -383,53 +422,89 @@ static int subr_bytes_to_printable(lua_State *L)
 
 static int subr_bytes_convert(lua_State *L)
 {
-  int base=luaL_checkint(L,1);
-  bytestring_t *bs = luaL_checkbytestring(L, 2);
-  bytestring_t *ret = luaL_newbytestring(L,base);
+  bytestring_t *bs = luaL_check_bytestring(L, 1);
+  int width=luaL_checkint(L,2);
+  bytestring_t *ret; 
+
+  if (width!=8 && width!=4 && width!=1)
+  	return luaL_error(L,"bytes.convert() only recognizes 8, 4 or 1 as parameter value.");
+
+  ret = bytestring_new(width);
 
   if (bytestring_convert(ret,bs)!=BYTESTRING_OK)
   {
-    lua_pop(L,1);
+    bytestring_free(ret);
     lua_pushnil(L);
+    return 1;
   }
+
+  lua_push_bytestring(L,ret);
   return 1;
 }
 
 static int subr_bytes_to_number(lua_State *L)
 {
-  bytestring_t *bs = luaL_checkbytestring(L, 1);
+  bytestring_t *bs = luaL_check_bytestring(L, 1);
   double ret=bytestring_to_number(bs);
   
   lua_pushnumber(L,ret);
   return 1;
 }
 
-static const struct luaL_reg byteslib_mt [] = {
+static int subr_bytes_format(lua_State *L)
+{
+  bytestring_t *bs = luaL_check_bytestring(L,1);
+  const char *format = lua_tostring(L,2);
+  char *s=bytestring_to_format(format,bs);
+  lua_pushstring(L,s);
+  free(s);
+  return 1;
+}
+
+/**********************************************
+ *
+ * LUA REGISTRATION
+ */
+
+
+static const struct luaL_reg byteslib_m [] = {
   {"__gc",subr_bytes_gc},
   {"__tostring",subr_bytes_tostring},
-  {"__index",subr_bytes_index},
-  {"__newindex",subr_bytes_newindex},
   {"__len",subr_bytes_len},
   {"__concat",subr_bytes_concat},
   {"__eq", subr_bytes_eq},
+
+  {"ipairs",subr_bytes_ipairs},
+  {"get",subr_bytes_get},
+  {"set",subr_bytes_set},
+  {"pad_left",subr_bytes_pad_left},
+  {"pad_right",subr_bytes_pad_right},
+  {"reverse",subr_bytes_reverse},
+  {"clone",subr_bytes_clone},
+  {"sub",subr_bytes_sub},
+  {"width",subr_bytes_width},
+  {"is_printable",subr_bytes_is_printable},
+  {"toprintable",subr_bytes_to_printable}, 
+  {"convert",subr_bytes_convert},
+  {"tonumber",subr_bytes_to_number},
+  {"format",subr_bytes_format},
   {NULL,NULL}
 };
 
-static const struct luaL_reg byteslib [] = {
+static const struct luaL_reg byteslib_f [] = {
   {"new", subr_bytes_new},
   {"new_from_chars",subr_bytes_new_from_chars},
-  {"assign",subr_bytes_assign},
-  {"append",subr_bytes_append},
-  {"insert",subr_bytes_insert},
-  {"concat",subr_bytes_concat},
+  {"concat", subr_bytes_concat},
+
+  {"ipairs",subr_bytes_ipairs},
+  {"get",subr_bytes_get},
+  {"set",subr_bytes_set},
   {"pad_left",subr_bytes_pad_left},
   {"pad_right",subr_bytes_pad_right},
-  {"invert",subr_bytes_invert},
+  {"reverse",subr_bytes_reverse},
   {"clone",subr_bytes_clone},
   {"sub",subr_bytes_sub},
-  {"remove",subr_bytes_remove},
   {"width",subr_bytes_width},
-  {"maxn",subr_bytes_maxn},
   {"is_printable",subr_bytes_is_printable},
   {"toprintable",subr_bytes_to_printable}, 
   {"convert",subr_bytes_convert},
@@ -441,8 +516,11 @@ static const struct luaL_reg byteslib [] = {
 int luaopen_bytes(lua_State* L) 
 {
   luaL_newmetatable(L, "bytes.type");
-  luaL_openlib(L, NULL, byteslib_mt, 0);
-  luaL_openlib(L, "bytes", byteslib, 0);
+  lua_pushstring(L, "__index");
+  lua_pushvalue(L, -2);  /* pushes the metatable */
+  lua_settable(L, -3);  /* metatable.__index = metatable */
+  luaL_openlib(L, NULL, byteslib_m, 0);
+  luaL_openlib(L, "bytes", byteslib_f, 0);
   return 1;
 }
 

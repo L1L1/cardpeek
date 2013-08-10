64c987bc7b588f55c039d1fc6e3388d7ace57403
L1L1 <L1L1@gmx.com>
2013-08-10 13:35:03 +0000

    Cleaned up the script a bit for Linux. Still needs testing on other platforms.

--- update_dot_cardpeek_dir.sh
+++ update_dot_cardpeek_dir.sh
@@ -2,9 +2,15 @@
 
 VERSION=`date +%s`
 
-SOURCE_DIR=`pwd`
+SOURCE_DST=`pwd`
 
-DOT_CARDPEEK_DIR="$SOURCE_DIR/dot_cardpeek_dir"
+if [ "x$CARDPEEK_DST" = "x" ]
+	DOT_CARDPEEK_SRC=~/.cardpeek
+else
+	DOT_CARDPEEK_SRC=$CARDPEEK_DST
+fi
+
+DOT_CARDPEEK_DST="$SOURCE_DST/dot_cardpeek_dir"
 
 MODIFIED='no'
 
@@ -23,31 +29,31 @@ in
 esac
 
 
-if [ -d ~/.cardpeek ]; then
+if [ -d "$DOT_CARDPEEK_SRC" ]; then
   
-  cd ~/.cardpeek
+  cd $DOT_CARDPEEK_SRC
 
   echo "## Retrieving new files:"
   IFS=$'\t\n' 
   for i in `find * \! -path replay/\*`;
   do
      if [ -d "$i" ]; then
-        if [ ! -e "$DOT_CARDPEEK_DIR/$i" ]; then
-            echo "!! creating dir $DOT_CARDPEEK_DIR/$i"
-            mkdir -p "$DOT_CARDPEEK_DIR/$i"
+        if [ ! -e "$DOT_CARDPEEK_DST/$i" ]; then
+            echo "!! creating dir $DOT_CARDPEEK_DST/$i"
+            mkdir -p "$DOT_CARDPEEK_DST/$i"
             MODIFIED='yes' 
         fi
-     elif (diff -q "$i" "$DOT_CARDPEEK_DIR/$i" | grep differ) &> /dev/null; then
-        if [ "$i" -nt "$DOT_CARDPEEK_DIR/$i" ]; then
-            echo ">> $i is newer than $DOT_CARDPEEK_DIR/$i"
+     elif (diff -q "$i" "$DOT_CARDPEEK_DST/$i" | grep differ) &> /dev/null; then
+        if [ "$i" -nt "$DOT_CARDPEEK_DST/$i" ]; then
+            echo ">> $i is newer than $DOT_CARDPEEK_DST/$i"
             if [ $TODO = "update" ]; then
-                cp -pRv "$i" "$DOT_CARDPEEK_DIR/$i"
+                cp -pRv "$i" "$DOT_CARDPEEK_DST/$i"
                 MODIFIED='yes' 
             fi
-        elif [ "$i" -ot "$DOT_CARDPEEK_DIR/$i" ]; then
-            echo "<< $i is older than $DOT_CARDPEEK_DIR/$i"
+        elif [ "$i" -ot "$DOT_CARDPEEK_DST/$i" ]; then
+            echo "<< $i is older than $DOT_CARDPEEK_DST/$i"
             if [ $TODO = "update" ]; then
-                cp -pRv "$DOT_CARDPEEK_DIR/$i" "$i"
+                cp -pRv "$DOT_CARDPEEK_DST/$i" "$i"
                 MODIFIED='yes' 
             fi
         fi
@@ -57,14 +63,14 @@ if [ -d ~/.cardpeek ]; then
   if [ "$TODO" = "update" ]; then
      if [ "$MODIFIED" = 'yes' ]; then
         echo "## Removing unecessary files and updating directory modfication time:"
-        rm -vf $DOT_CARDPEEK_DIR/replay/* 
-        touch $DOT_CARDPEEK_DIR
+        rm -vf $DOT_CARDPEEK_DST/replay/* 
+        touch $DOT_CARDPEEK_DST
 
         echo "## Set version info:"
         echo "   Version ID is now $VERSION"
-        echo $VERSION > $DOT_CARDPEEK_DIR/version
-        echo $VERSION > ~/.cardpeek/version
-        cat > "$SOURCE_DIR/script_version.h" << __EOF
+        echo $VERSION > $DOT_CARDPEEK_DST/version
+        echo $VERSION > $DOT_CARDPEEK_SRC/version
+        cat > "$SOURCE_DST/script_version.h" << __EOF
 #ifndef SCRIPT_VERSION_H
 #define SCRIPT_VERSION_H
 

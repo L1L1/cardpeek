563a7a5516d065cefa4f935e1c2b8ec83cd61a88
L1L1@gmx.com <L1L1@gmx.com@b6bb1ec2-7570-11de-9207-fb4211ab666b>
2013-11-24 13:30:58 +0000

    reindented file

--- trunk/lua_card.c
+++ trunk/lua_card.c
@@ -1,4 +1,4 @@
-/********************************************************************** 
+/**********************************************************************
 *
 * This file is part of Cardpeek, the smart card reader utility.
 *
@@ -28,165 +28,169 @@
 #include "lua_ext.h"
 #include "gui.h"
 
-cardreader_t* READER=NULL;
+cardreader_t *READER=NULL;
 
-void luax_set_card_reader(cardreader_t* r)
+void luax_set_card_reader(cardreader_t *r)
 {
-  READER = r;
+    READER = r;
 }
 
-static int subr_card_connect(lua_State* L)
+static int subr_card_connect(lua_State *L)
 {
-  if(!cardreader_connect(READER,PROTOCOL_T0 | PROTOCOL_T1))
-    lua_pushboolean(L,0);
-  else
+    if(!cardreader_connect(READER,PROTOCOL_T0 | PROTOCOL_T1))
+        lua_pushboolean(L,0);
+    else
+        lua_pushboolean(L,1);
+    return 1;
+}
+
+static int subr_card_disconnect(lua_State *L)
+{
+    cardreader_disconnect(READER);
+
     lua_pushboolean(L,1);
-  return 1;
+    return 1;
 }
 
-static int subr_card_disconnect(lua_State* L)
+static int subr_card_warm_reset(lua_State *L)
 {
-  cardreader_disconnect(READER);
-  
-  lua_pushboolean(L,1);
-  return 1;
+    if (cardreader_warm_reset(READER))
+        lua_pushboolean(L,1);
+    else
+        lua_pushboolean(L,0);
+    return 1;
 }
 
-static int subr_card_warm_reset(lua_State* L)
+static int subr_card_last_atr(lua_State *L)
 {
-  if (cardreader_warm_reset(READER))
-    lua_pushboolean(L,1);
-  else
-    lua_pushboolean(L,0);
-  return 1;
+    const bytestring_t *atr = cardreader_last_atr(READER);
+
+    lua_push_bytestring(L,bytestring_duplicate(atr));
+    return 1;
 }
 
-static int subr_card_last_atr(lua_State* L)
+static int subr_card_info(lua_State *L)
 {
-  const bytestring_t *atr = cardreader_last_atr(READER);
+    char **info=cardreader_get_info(READER);
+    int index;
 
-  lua_push_bytestring(L,bytestring_duplicate(atr));
-  return 1;
+    lua_newtable(L);
+
+    for (index=0; info[index]!=NULL; index+=2)
+    {
+        lua_pushstring(L,info[index]);
+        lua_pushstring(L,info[index+1]);
+        lua_settable(L,-3);
+        g_free(info[index]);
+        g_free(info[index+1]);
+    }
+    g_free(info);
+
+    /* this update is needed since cardreader_get_info is silent upon success */
+    gui_update(0);
+    return 1;
 }
 
-static int subr_card_info(lua_State* L)
+static int subr_card_send(lua_State *L)
 {
-  char **info=cardreader_get_info(READER);
-  int index;
+    bytestring_t *command = luaL_check_bytestring(L,1);
+    bytestring_t *result = bytestring_new(8);
+    unsigned short SW;
 
-  lua_newtable(L);
+    SW=cardreader_transmit(READER,command,result);
 
-  for (index=0;info[index]!=NULL;index+=2)
-  {
-    lua_pushstring(L,info[index]);
-    lua_pushstring(L,info[index+1]);
-    lua_settable(L,-3);
-    g_free(info[index]);
-    g_free(info[index+1]);
-  }
-  g_free(info);
-
-  /* this update is needed since cardreader_get_info is silent upon success */
-  gui_update(0);
-  return 1;
+    lua_pushinteger(L,SW);
+    lua_push_bytestring(L,result);
+    return 2;
 }
 
-static int subr_card_send(lua_State* L)
+static int subr_card_set_command_interval(lua_State *L)
 {
-  bytestring_t *command = luaL_check_bytestring(L,1);
-  bytestring_t *result = bytestring_new(8);
-  unsigned short SW;
-
-  SW=cardreader_transmit(READER,command,result);
-
-  lua_pushinteger(L,SW);
-  lua_push_bytestring(L,result);
-  return 2;
+    unsigned interval=(unsigned)lua_tointeger(L,1);
+    cardreader_set_command_interval(READER,interval);
+    return 0;
 }
 
-static int subr_card_set_command_interval(lua_State* L)
+static int subr_card_make_file_path(lua_State *L)
 {
-  unsigned interval=(unsigned)lua_tointeger(L,1);
-  cardreader_set_command_interval(READER,interval);
-  return 0;
+    int path_type;
+    bytestring_t *file_path = bytestring_new(8);
+    const char *path;
+
+    if (lua_isnoneornil(L,1))
+        path = "";
+    else
+        path = lua_tostring(L,1);
+
+    if (iso7816_make_file_path(file_path,&path_type,path)!=ISO7816_OK)
+    {
+        bytestring_free(file_path);
+        lua_pushnil(L);
+        lua_pushnil(L);
+        log_printf(LOG_ERROR,"Could not parse card file path '%s'",path);
+    }
+    else
+    {
+        lua_push_bytestring(L,file_path);
+        lua_pushinteger(L,path_type);
+    }
+    return 2;
 }
 
-static int subr_card_make_file_path(lua_State* L)
+static int subr_card_log_save(lua_State *L)
 {
-  int path_type;
-  bytestring_t *file_path = bytestring_new(8);
-  const char *path;
+    const char *filename;
 
-  if (lua_isnoneornil(L,1))
-    path = "";
-  else
-    path = lua_tostring(L,1);
+    if (lua_isnoneornil(L,1))
+        return luaL_error(L,"Expecting one parameter: a filename (string)");
 
-  if (iso7816_make_file_path(file_path,&path_type,path)!=ISO7816_OK)
-  {
-    bytestring_free(file_path);
-    lua_pushnil(L);
-    lua_pushnil(L);
-    log_printf(LOG_DEBUG,"Could not parse path %s",path);
-  }
-  else {
-    lua_push_bytestring(L,file_path);
-    lua_pushinteger(L,path_type);
-  }
-  return 2;
+    filename= lua_tostring(L,1);
+
+    if (cardreader_log_save(READER,filename)!=SMARTCARD_OK)
+    {
+        log_printf(LOG_ERROR,"Could not write data to '%s'",filename);
+        lua_pushboolean(L,0);
+    }
+    else
+    {
+        log_printf(LOG_INFO,"Wrote card data to '%s'",filename);
+        lua_pushboolean(L,1);
+    }
+    return 1;
 }
 
-static int subr_card_log_save(lua_State* L)
+static int subr_card_log_clear(lua_State *L)
 {
-  const char *filename;
+    UNUSED(L);
 
-  if (lua_isnoneornil(L,1))
-    return luaL_error(L,"Expecting one parameter: a filename (string)");
-
-  filename= lua_tostring(L,1);
-
-  if (cardreader_log_save(READER,filename)!=SMARTCARD_OK)
-  {
-    log_printf(LOG_ERROR,"Could not write data to '%s'",filename);
-    lua_pushboolean(L,0);
-  }
-  else
-  {
-    log_printf(LOG_INFO,"Wrote card data to '%s'",filename);
-    lua_pushboolean(L,1);
-  }
-  return 1;  
+    cardreader_log_clear(READER);
+    return 0;
 }
 
-static int subr_card_log_clear(lua_State* L)
+static const struct luaL_Reg cardlib [] =
 {
-  UNUSED(L);
-
-  cardreader_log_clear(READER);
-  return 0;  
-}
-
-static const struct luaL_Reg cardlib [] = {
-  {"connect", subr_card_connect },
-  {"disconnect", subr_card_disconnect },
-  {"warm_reset", subr_card_warm_reset },
-  {"last_atr", subr_card_last_atr },
-  {"info", subr_card_info },
-  {"send", subr_card_send },
-  {"set_command_interval", subr_card_set_command_interval },
-  {"make_file_path", subr_card_make_file_path },
-  {"log_clear", subr_card_log_clear },
-  {"log_save", subr_card_log_save },
-  {NULL, NULL}  /* sentinel */
+    {"connect", subr_card_connect },
+    {"disconnect", subr_card_disconnect },
+    {"warm_reset", subr_card_warm_reset },
+    {"last_atr", subr_card_last_atr },
+    {"info", subr_card_info },
+    {"send", subr_card_send },
+    {"set_command_interval", subr_card_set_command_interval },
+    {"make_file_path", subr_card_make_file_path },
+    {"log_clear", subr_card_log_clear },
+    {"log_save", subr_card_log_save },
+    {NULL, NULL}  /* sentinel */
 };
 
-int luaopen_card(lua_State* L)
+int luaopen_card(lua_State *L)
 {
-  
-  luaL_newlib(L, cardlib);
-  lua_pushstring(L, "CLA"); lua_pushinteger(L, 0); lua_settable(L,-3);
-  lua_setglobal(L,"card");
 
-  return 1;
+    luaL_newlib(L, cardlib);
+    lua_pushstring(L, "CLA");
+    lua_pushinteger(L, 0);
+    lua_settable(L,-3);
+    lua_setglobal(L,"card");
+
+    return 1;
 }
 

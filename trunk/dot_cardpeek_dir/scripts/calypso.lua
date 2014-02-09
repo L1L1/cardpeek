526a43a838bd9bec4fae03e16073d3172312689f
L1L1@gmx.com <L1L1@gmx.com@b6bb1ec2-7570-11de-9207-fb4211ab666b>
2014-02-09 09:36:52 +0000

    Added new file IDs to scan, as suggested by P. Gueulle

--- trunk/dot_cardpeek_dir/scripts/calypso.lua
+++ trunk/dot_cardpeek_dir/scripts/calypso.lua
@@ -52,7 +52,7 @@ function bytes.is_all(bs,byte)
 	return true
 end
 
-
+--[[
 LFI_LIST = {
   { "ICC",              "/0002",      "file" },
   { "ID",               "/0003",      "file" },
@@ -74,6 +74,69 @@ LFI_LIST = {
   { "Contract list",    "/2000/2050", "file" },
   { "Counters",         "/2000/2069", "file" },
   { "Holder Extended",  "/3F1C",      "file" }
+}
+--]]
+LFI_LIST = {
+
+  { "AID",              "/3F04",      "file" },
+  { "ICC",              "/0002",      "file" },
+  { "ID",               "/0003",      "file" },
+  { "Holder Extended",  "/3F1C",      "file" },
+  { "Display / Free",   "/2F10",      "file" },
+  
+  { "Ticketing",        "/2000",    "folder" },
+  { "AID",              "/2000/2004", "file" },
+  { "Environment",      "/2000/2001", "file" },
+  { "Holder",           "/2000/2002", "file" }, 
+  { "Event logs",       "/2000/2010", "file" },
+  { "Contracts",        "/2000/2020", "file" },
+  { "Contracts",        "/2000/2030", "file" },
+  { "Counters",         "/2000/202A", "file" },
+  { "Counters",         "/2000/202B", "file" },
+  { "Counters",         "/2000/202C", "file" },
+  { "Counters",         "/2000/202D", "file" },
+  { "Counters",         "/2000/202E", "file" },
+  { "Counters",         "/2000/202F", "file" },
+  { "Counters",         "/2000/2060", "file" },
+  { "Counters",         "/2000/2061", "file" },
+  { "Counters",         "/2000/2062", "file" },
+  { "Counters",         "/2000/2069", "file" },
+  { "Counters",         "/2000/206A", "file" },
+  { "Special events",   "/2000/2040", "file" },
+  { "Contract list",    "/2000/2050", "file" },
+  { "Free",             "/2000/20F0", "file" },
+ 
+  { "MPP",              "/3100",    "folder" },
+    { "AID",            "/3100/3104", "file" },
+    { "Public Param.",  "/3100/3102", "file" },
+    { "Log",            "/3100/3115", "file" },
+    { "Contracts",      "/3100/3120", "file" },
+    { "Counters",       "/3100/3113", "file" },
+    { "Counters",       "/3100/3123", "file" },
+    { "Counters",       "/3100/3133", "file" },
+    { "Counters",       "/3100/3169", "file" },
+    { "Miscellaneous",  "/3100/3150", "file" },
+    { "Free",           "/3100/31F0", "file" },
+        
+  { "RT2",              "/2100",    "folder" },
+    { "AID",            "/2100/2104", "file" },
+    { "Environment",    "/2100/2101", "file" },
+    { "Event logs",     "/2100/2110", "file" },
+    { "Contract list",  "/2100/2150", "file" },
+    { "Contracts",      "/2100/2120", "file" },
+    { "Counters",       "/2100/2169", "file" },
+    { "Special events", "/2100/2140", "file" },
+    { "Free",           "/2100/21F0", "file" },
+      
+  { "EP",               "/1000",    "folder" },
+   { "AID",             "/1000/1004", "file" },
+   { "Load Log",        "/1000/1014", "file" },
+   { "Purchase Log",    "/1000/1015", "file" },
+   
+  { "eTicket",          "/8000",    "folder" },
+   { "AID",             "/8000/8004", "file" },
+   { "Preselection",    "/8000/8030", "file" },
+   { "Event logs",      "/8000/8010", "file" }
 }
 
 function calypso_select(ctx,desc,path,klass)
